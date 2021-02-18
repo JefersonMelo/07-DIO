f2324a641021f424056a06a0a9dd380f148f87fc
Jeferson Melo <jefsdemelo@gmail.com>
2021-02-18 14:27:09 -0300

    commit interface IRepositorio e seus atributos.

---
+++ MRV/CadastroSeries/Interfaces/IRepositorio.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CadastroSeries.Interfaces
+{
+    public interface IRepositorio<T>
+    {
+        // Padrão de Repositório, Design Patterns
+        List<T> Lista( );
+        T RetornarId(int id);
+        void Inserir( T objeto );
+        void Excuir( int id );
+        void Atualizar( int id, T objeto );
+        int ProximoId( );
+
+    }
+}
