a3ed60b4b895935cf2f7aa884d3b54ab921f0b6e
Jeferson Melo <jefsdemelo@gmail.com>
2021-02-19 10:45:25 -0300

    Commit Classe Filme e RepoiterioFilmes

--- MRV/CadastroSeries/Classes/Filme.cs
+++ MRV/CadastroSeries/Classes/Filme.cs
@@ -1,25 +1,27 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace CadastroSeries
 {
-    public class Serie : EntidadeBase
+    public class Filme : EntidadeBase
     {
-        //Atributos
+        //public Filme( ) { }
+
         private Genero Genero { get; set; }
         private string Titulo { get; set; }
         private string Descricao { get; set; }
         private int Ano { get; set; }
         private bool Excluido { get; set; }
 
-        //Métodos
-        public Serie( int id, Genero genero, string titulo, string descricao, int ano )
+        public Filme( int Id, Genero genero, string titulo, string descricao, int ano, bool excluido )
         {
-            this.Id = id;
+            this.Id = Id;
             this.Genero = genero;
             this.Titulo = titulo;
             this.Descricao = descricao;
             this.Ano = ano;
-            this.Excluido = false;
+            this.Excluido = excluido;
         }
 
         public override string ToString( )
@@ -28,13 +30,6 @@ namespace CadastroSeries
              * Environment.NewLine 
              * https://docs.microsoft.com/en-us/dotnet/api/system.environment.newline?view=netcore-3.1
              */
-            //string retorno = "";
-            //retorno += "Gênero: " + this.Genero + Environment.NewLine;
-            //retorno += "Titulo: " + this.Titulo + Environment.NewLine;
-            //retorno += "Descrição: " + this.Descricao + Environment.NewLine;
-            //retorno += "Ano de Início: " + this.Ano + Environment.NewLine;
-            //retorno += "Excluido: " + this.Excluido;
-            //return retorno;
             return $"\nGênero: {this.Genero + Environment.NewLine}" +
                    $"Título: {this.Titulo + Environment.NewLine}" +
                    $"Descrição: {this.Descricao + Environment.NewLine}" +
---
+++ MRV/CadastroSeries/Classes/RepositorioFilmes.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CadastroSeries
+{
+    public class RepositorioFilmes : IRepositorio<Filme>
+    {
+        private List<Filme> listaFilmes = new List<Filme>();
+
+        public void Atualizar( int id, Filme objeto )
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Excluir( int id )
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Inserir( Filme objeto )
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Filme> Lista( )
+        {
+            throw new NotImplementedException();
+        }
+
+        public int ProximoId( )
+        {
+            throw new NotImplementedException();
+        }
+
+        public Filme RetornarId( int id )
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
