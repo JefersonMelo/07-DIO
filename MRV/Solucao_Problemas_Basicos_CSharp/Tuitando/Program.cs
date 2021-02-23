0de32a584c98d2ca850d58e4dbc68f7860b3613e
Jeferson Melo <jefsdemelo@gmail.com>
2021-02-22 21:34:34 -0300

    commit chaves nas condicionais

--- MRV/Solucao_Problemas_Basicos_CSharp/Tuitando/Program.cs
+++ MRV/Solucao_Problemas_Basicos_CSharp/Tuitando/Program.cs
@@ -9,9 +9,13 @@ namespace Tuitando
             string v = Console.ReadLine();
             char[] arr = v.ToCharArray();
             if (arr.Length <= 140) //complete a condicional
+            {
                 Console.WriteLine("TWEET");
+            }
             else
+            {
                 Console.WriteLine("MUTE");
+            }
         }
     }
 }
