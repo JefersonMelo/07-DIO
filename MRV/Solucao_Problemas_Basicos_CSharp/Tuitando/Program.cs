86b3a9c6006b8a1cf0a4c44d70aaaa0f7e6c83a9
Jeferson Melo <jefsdemelo@gmail.com>
2021-02-22 21:32:54 -0300

    commit final

---
+++ MRV/Solucao_Problemas_Basicos_CSharp/Tuitando/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Tuitando
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string v = Console.ReadLine();
+            char[] arr = v.ToCharArray();
+            if (arr.Length <= 140) //complete a condicional
+                Console.WriteLine("TWEET");
+            else
+                Console.WriteLine("MUTE");
+        }
+    }
+}
