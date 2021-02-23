5908346e1773bb973c5e8a4458df39620cba94fb
Jeferson Melo <jefsdemelo@gmail.com>
2021-02-22 22:01:47 -0300

    add comentrários

--- MRV/Solucao_Problemas_Basicos_CSharp/Tuitando/Program.cs
+++ MRV/Solucao_Problemas_Basicos_CSharp/Tuitando/Program.cs
@@ -4,6 +4,20 @@ namespace Tuitando
 {
     class Program
     {
+        /*
+          microblog Twitter é conhecido por limitar as postagens em 140 caracteres. Conferir se um texto vai caber em um tuíte é sua tarefa.
+        Entrada: A entrada é uma linha de texto T (1 ≤ |T| ≤ 500).
+        Saída: A saída é dada em uma única linha. Ela deve ser "TWEET" (sem as aspas) se a linha de texto T tem até 140 caracteres. 
+        Se T tem mais de 140 caracteres, a saída deve ser "MUTE".
+        Exemplo de Entrada      Exemplo de Saída
+        RT @TheEllenShow:       TWEET
+        If only Bradley's 
+        arm was longer. 
+        Best photo ever. 
+        #oscars pic.twitter.com/C9U5NOtGap
+
+         */
+
         static void Main(string[] args)
         {
             string v = Console.ReadLine();
