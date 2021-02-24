b5626f228b7de10772eaae04aa6f81b8b3026d20
Jeferson Melo <jefsdemelo@gmail.com>
2021-02-24 14:37:12 -0300

    commit do projeto aurora no inicio do clone

---
+++ MRV/01-Visita_Na_Feira/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01_Visita_Na_Feira
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
