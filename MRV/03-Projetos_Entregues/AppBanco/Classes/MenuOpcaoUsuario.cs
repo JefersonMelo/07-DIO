caed0176f4518bbff593017e88b58d13cbe903be
Jeferson Melo <jefsdemelo@gmail.com>
2021-02-27 20:19:33 -0300

    commit classe MenuOpcaoUsuario

---
+++ MRV/03-Projetos_Entregues/AppBanco/Classes/MenuOpcaoUsuario.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppBanco
+{
+    class MenuOpcaoUsuario
+    {
+        private char OpcaoUsuario { get; set; }
+
+        public MenuOpcaoUsuario() { }
+
+        public MenuOpcaoUsuario(char opcaoUsuario)
+        {
+            this.OpcaoUsuario = opcaoUsuario;
+        }
+
+        // Menu de opções de escolha do usuário
+        public string ImprimirMenu()
+        {
+            return $"\nInforme Uma Opção:\n" +
+                   $"1- Listar Contas\n" +
+                   $"2- Inserir Nova Conta\n" +
+                   $"3- Transferir\n" +
+                   $"4- Sacar\n" +
+                   $"5- Depositar\n" +
+                   $"C- Limpar Tela\n" +
+                   $"X- Sair";
+        }
+    }
+}
