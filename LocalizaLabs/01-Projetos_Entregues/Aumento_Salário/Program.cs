1b2325de0ff6e3d4c313d526c763897b3ce781ed
Jeferson Melo <jefsdemelo@gmail.com>
2021-03-03 18:37:20 -0300

    commit final

--- LocalizaLabs/01-Projetos_Entregues/Aumento_Salário/Program.cs
+++ LocalizaLabs/01-Projetos_Entregues/Aumento_Salário/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace Aumento_Salário
 {
@@ -15,51 +16,44 @@ namespace Aumento_Salário
          */
         static void Main(string[] args)
         {
-            double salario, reajuste, novoSalario, percentual;
-            salario = Convert.ToDouble(Console.ReadLine());
+            double reajuste, p;
+            p = 1.0 / 100.0;
+            reajuste = 0;
+            double salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            //insira os valores corretos de acordo com o enunciado
-
-            if ( salario >= 0.00 && salario <= 000.00 )
+            if ( salario <= 400.00 )
             {
-                reajuste = salario * 0.00;
-                novoSalario = salario + reajuste;
-                percentual = ( ( novoSalario - salario ) * 000 ) / salario;
-                Console.WriteLine("Novo salario: {0:0.00}", novoSalario);
-                Console.WriteLine("Reajuste ganho: {0:0.00}", reajuste);
-                Console.WriteLine("Em percentual: {0} %", percentual);
+                reajuste = salario * ( 15.0 * p );
+                salario += salario * ( 15.0 * p );
+                p *= 15.0 * 100.0;
             }
-            else if ( salario > 000.00 && salario <= 000.00 )
+            else if ( salario > 400.00 && salario <= 800.00 )
             {
-                reajuste = salario * 0.00;
-                novoSalario = salario + reajuste;
-                percentual = ( ( novoSalario - salario ) * 000 ) / salario;
-                Console.WriteLine("Novo salario: {0:0.00}", novoSalario);
-                Console.WriteLine("Reajuste ganho: {0:0.00}", reajuste);
-                Console.WriteLine("Em percentual: {0} %", percentual);
+                reajuste = salario * ( 12.0 * p );
+                salario += salario * ( 12.0 * p );
+                p *= 12.0 * 100.0;
             }
-            else if ( salario > 000.00 && salario <= 000.00 )
+            else if ( salario > 800.00 && salario <= 1200.00 )
             {
-                reajuste = salario * 0.00;
-                novoSalario = salario + reajuste;
-                percentual = ( ( novoSalario - salario ) * 000 ) / salario;
-                Console.WriteLine("Novo salario: {0:0.00}", novoSalario);
-                Console.WriteLine("Reajuste ganho: {0:0.00}", reajuste);
-                Console.WriteLine("Em percentual: {0} %", percentual);
+                reajuste = salario * ( 10.0 * p );
+                salario += salario * ( 10.0 * p );
+                p *= 10.0 * 100.0;
             }
-            else if ( salario > 000.00 && salario <= 000.00 )
+            else if ( salario > 1200.00 && salario <= 2000.00 )
             {
-                reajuste = salario * 0.00;
-                novoSalario = salario + reajuste;
-                percentual = ( ( novoSalario - salario ) * 000 ) / salario;
-                Console.WriteLine("Novo salario: {0:0.00}", novoSalario);
-                Console.WriteLine("Reajuste ganho: {0:0.00}", reajuste);
-                Console.WriteLine("Em percentual: {0} %", percentual);
+                reajuste = salario * ( 7.0 * p );
+                salario += salario * ( 7.0 * p );
+                p *= 7.0 * 100.0;
             }
-            else
+            else if ( salario > 2000.00 )
             {
-                //complete o codigo
+                reajuste = salario * ( 4.0 * p );
+                salario += salario * ( 4.0 * p );
+                p *= 4.0 * 100.0;
             }
+            Console.WriteLine($"Novo salario: {salario.ToString("F2", CultureInfo.InvariantCulture)}\n" +
+                              $"Reajuste ganho: {reajuste.ToString("F2", CultureInfo.InvariantCulture)}\n" +
+                              $"Em percentual: {p} %");
         }
     }
 }
