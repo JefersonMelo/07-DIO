0a769ff659973624d6c2968f73f3fcf8815f4573
Jeferson Melo <jefsdemelo@gmail.com>
2021-03-03 17:39:59 -0300

    commit final

---
+++ LocalizaLabs/01-Projetos_Entregues/Calcular_Consumo_Medio/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Calcular_Consumo_Medio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int distancia;
+            double combustivelGasto, consumoMedio;
+
+            distancia = Convert.ToInt32(Console.ReadLine());
+            combustivelGasto = Convert.ToDouble(Console.ReadLine());
+
+            consumoMedio = distancia / combustivelGasto; //atribua a formula para que o codigo funcione corretamente
+
+            Console.WriteLine("{0:0.000} km/l", consumoMedio);
+        }
+    }
+}
