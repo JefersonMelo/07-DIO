3c5a42b817621ec8cbfe27b54221193dfc291c08
Jeferson Melo <jefsdemelo@gmail.com>
2021-03-03 17:42:33 -0300

    commit final

--- LocalizaLabs/01-Projetos_Entregues/Calcular_Consumo_Medio/Program.cs
+++ LocalizaLabs/01-Projetos_Entregues/Calcular_Consumo_Medio/Program.cs
@@ -4,6 +4,20 @@ namespace Calcular_Consumo_Medio
 {
     class Program
     {
+        /*
+        Desafio: Você deve calcular o consumo médio de um automóvel onde será informada a distância total percorrida (em Km) 
+        e o total de combustível consumido (em litros).
+        Entrada: Você receberá dois valores: um valor inteiro X com a distância total percorrida (em Km), e um valor real Y 
+        que representa o total de combustível consumido, com um dígito após o ponto decimal.
+        Saída: Exiba o valor que representa o consumo médio do automóvel (3 casas após a vírgula), incluindo no final a mensagem "km/l".
+        Exemplo de Entrada 	    Exemplo de Saída
+        500                     14.286 km/l
+        35.0
+
+        2254                    18.119 km/l
+        124.4
+         */
+
         static void Main(string[] args)
         {
             int distancia;
