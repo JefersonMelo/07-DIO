303c5cf12a5aa46dd95eec38e519ab9cf6c9ad5b
Jeferson Melo <jefsdemelo@gmail.com>
2021-03-05 17:08:13 -0300

    commit inicial

---
+++ LocalizaLabs/01-Projetos_Entregues/03-Praticando_Programacao/Compras_Supermercado/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Compras_Supermercado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
