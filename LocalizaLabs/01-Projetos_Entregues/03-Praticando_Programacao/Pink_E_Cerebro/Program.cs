227831e17efed5989b47fede48cc3a4081cf8648
Jeferson Melo <jefsdemelo@gmail.com>
2021-03-05 20:05:06 -0300

    commit final

--- LocalizaLabs/01-Projetos_Entregues/03-Praticando_Programacao/Pink_E_Cerebro/Program.cs
+++ LocalizaLabs/01-Projetos_Entregues/03-Praticando_Programacao/Pink_E_Cerebro/Program.cs
@@ -24,7 +24,37 @@ namespace Pink_E_Cerebro
          */
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = int.Parse(Console.ReadLine());
+            int num, 
+                cont2 = 0, 
+                cont3 = 0,
+                cont4 = 0,
+                cont5 = 0;
+            string[] numString = Console.ReadLine().Split();
+            for ( int i = 0; i < n; i++ )
+            {
+                num = int.Parse(numString[i]);
+                if ( num % 2 == 0 )
+                {
+                    cont2++;
+                }
+                if ( num % 3 == 0 )
+                {
+                    cont3++;
+                }
+                if ( num % 4 == 0 )
+                {
+                    cont4++;
+                }
+                if ( num % 5 == 0 )
+                {
+                    cont5++;
+                }
+            }
+            Console.WriteLine($"{cont2} Multiplo(s) de 2\n" +
+                              $"{cont3} Multiplo(s) de 3\n" +
+                              $"{cont4} Multiplo(s) de 4\n" +
+                              $"{cont5} Multiplo(s) de 5");
         }
     }
 }
