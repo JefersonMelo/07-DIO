60f13b2b87493b219349ebfdc3ac000130516669
Jeferson Melo <jefsdemelo@gmail.com>
2021-03-06 21:02:58 -0300

    commit final

--- .Net_Fundamentals/Introducao_Programacao_Com_.NET/Dividindo_X_Por_Y/Program.cs
+++ .Net_Fundamentals/Introducao_Programacao_Com_.NET/Dividindo_X_Por_Y/Program.cs
@@ -1,12 +1,46 @@
 ﻿using System;
+using System.Globalization;
 
 namespace Dividindo_X_Por_Y
 {
     class Program
     {
+        /*
+        Escreva um algoritmo que leia 2 números e imprima o resultado da divisão do primeiro pelo segundo. 
+        Caso não for possível mostre a mensagem “divisao impossivel” para os valores em questão.
+        Entrada: A entrada contém um número inteiro N. Este N será a quantidade de pares de valores inteiros 
+        (X e Y) que serão lidos em seguida.
+        Saída: Para cada caso mostre o resultado da divisão com um dígito após o ponto decimal, ou “divisao impossivel” 
+        caso não seja possível efetuar o cálculo.
+        Obs.: Cuide que a divisão entre dois inteiros em algumas linguagens como o C e C++ gera outro inteiro. Utilize cast :)
+        Exemplo de Entrada 	Exemplo de Saída
+        3                   -1.5
+        3 -2                divisao impossivel
+        -8 0                0.0
+        0 8
+        */
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double r;
+            int n, x, y;
+            n = int.Parse(Console.ReadLine());
+            for ( int i = 0; i < n; i++ )
+            {
+                string[] s = Console.ReadLine().Split();
+                x = int.Parse(s[0], CultureInfo.InvariantCulture);
+                y = int.Parse(s[1], CultureInfo.InvariantCulture);
+
+                if ( y == 0 || x < 0 )
+                {
+                    Console.WriteLine("divisao impossivel");
+                }
+                else
+                {
+                    r = ( double )x / y;
+                    Console.WriteLine($"{r.ToString("F1", CultureInfo.InvariantCulture)}");
+                }
+            }
         }
     }
 }
