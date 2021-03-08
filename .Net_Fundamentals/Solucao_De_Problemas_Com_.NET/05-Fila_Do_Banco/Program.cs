0bb4ef84662c66d0e052085ba06995261c818eeb
Jeferson Melo <jefsdemelo@gmail.com>
2021-03-08 18:07:43 -0300

    commit inicial

---
+++ .Net_Fundamentals/Solucao_De_Problemas_Com_.NET/05-Fila_Do_Banco/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Fila_Do_Banco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
