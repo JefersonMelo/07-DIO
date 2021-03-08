2523d06bba33eeca257b7af29e1593bcfd53b903
Jeferson Melo <jefsdemelo@gmail.com>
2021-03-08 17:09:34 -0300

    commit usando expressões regulares. não aceito.

---
+++ .Net_Fundamentals/Solucao_De_Problemas_Com_.NET/Validador_De_Senhas_Com_Requisitos/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Validador_De_Senhas_Com_Requisitos
+{
+    class Program
+    {
+        static void Main( )
+        {
+            string senha = Console.ReadLine();
+            var contMinusculo = Regex.Matches(senha, "[a-z]").Count;
+            var contMaiusculo = Regex.Matches(senha, "[A-Z]").Count;
+            var contNumero = Regex.Matches(senha, "[0-9]").Count;
+
+            while ( senha != "" )
+            {
+                if ( senha.Length >= 6
+                  && senha.Length <= 32
+                  && contMinusculo > 0
+                  && contMaiusculo > 0
+                  && contNumero > 0
+                  && ( contMinusculo + contMaiusculo + contNumero == senha.Length ) )
+                {
+                    Console.WriteLine("Senha valida.");
+                }
+                else
+                {
+                    Console.WriteLine("Senha invalida.");
+                }
+
+                senha = Console.ReadLine();
+                contMinusculo = Regex.Matches(senha, "[a-z]").Count;
+                contMaiusculo = Regex.Matches(senha, "[A-Z]").Count;
+                contNumero = Regex.Matches(senha, "[0-9]").Count;
+            }
+
+
+        }
+    }
+}
+
