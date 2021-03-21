2328d766c40fd7d4f6cec7de9188b9786a475417
Jeferson Melo <jefsdemelo@gmail.com>
2021-03-20 23:31:56 -0300

    commit das novas páginas de filmes, séres, docs e etc. Carrossel página inicial altomático, com botão em cada poster de play e info.

--- 04-Introduca-criacao-de-websites-com-HTML5-CSS3/03-Recriando-a-Interface-do-Netflix/js/main.js
+++ 04-Introduca-criacao-de-websites-com-HTML5-CSS3/03-Recriando-a-Interface-do-Netflix/js/main.js
@@ -13,13 +13,4 @@ $('.owl-carousel').owlCarousel({
             items:5
         }
     },
-    autoplay:true,
-    autoplayTimeout:3000,
-    autoplayHoverPause:true
-});
-$('.play').on('click',function(){
-    owl.trigger('play.owl.autoplay',[3000])
-})
-$('.stop').on('click',function(){
-    owl.trigger('stop.owl.autoplay')
 })
