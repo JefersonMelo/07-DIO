3e63f228e58ac6153adaf419aaa8a569cd464f10
Jeferson Melo <jefsdemelo@gmail.com>
2021-04-20 11:37:35 -0300

    remoção de arquivos inuteis

--- 05-JavaScript-Game-Developer/03-Introducao-a-Programacao-com-JavaScript/03-Folha-de-Pagamento/js/Folha-de-Pagamento.js
+++ 05-JavaScript-Game-Developer/03-Introducao-a-Programacao-com-JavaScript/03-Folha-de-Pagamento/js/Folha-de-Pagamento.js
@@ -1,13 +1,6 @@
-var valor1 = prompt("Valor 1");
-var valor2 = prompt("Valor 2");
-var valor3 = prompt("Valor 3");
-var salary;
-
-function valorSalario(valor2, valor3) {
-    return salary = parseFloat(valor2 * valor3).toFixed(2);// Digite aqui o calculo do salário
-}
-function resposta() {
-    document.getElementById("resposta").innerHTML = "NUMBER = " + valor1 + "\nSALARY = U$ " + valorSalario(valor2, valor3);
-}
+let valor1 = parseInt(gets());
+let valor2 = parseInt(gets());
+let valor3 = parseFloat(gets());
+let salary = parseFloat(valor2 * valor3).toFixed(2); // Digite aqui o calculo do salário
 console.log("NUMBER = " + valor1);
 console.log("SALARY = U$ " + salary);
