3772d4a6d355a7f1d4ff9c1f074e11603e560a66
Jeferson Melo <jefsdemelo@gmail.com>
2021-04-21 12:08:55 -0300

    criação da mecanica do cactus

--- 05-JavaScript-Game-Developer/07-Dino-Game/js/script.js
+++ 05-JavaScript-Game-Developer/07-Dino-Game/js/script.js
@@ -1,4 +1,5 @@
 const dino = document.querySelector('.dino');
+const background = document.querySelector('.background');
 
 var isJumping = false;
 
@@ -37,4 +38,27 @@ function jump() {
     }, 20);
 }
 
+function createCactus() {
+    const cactus = document.createElement('div');
+    var cactusPisition = 1000;
+    var randomTime = Math.random() * 6000;
+
+    cactus.classList.add('cactus');
+    cactus.style.left = 1000 + 'px';
+    background.appendChild(cactus);
+
+    var leftInterval = setInterval(() => {
+        if (cactusPisition < -60) {
+            clearInterval(leftInterval);
+            background.removeChild(cactus);
+        } else {
+            cactusPisition -= 10;
+            cactus.style.left = cactusPisition + 'px';
+        }
+    }, 20);
+
+    setTimeout(createCactus, randomTime);
+}
+
+createCactus();
 document.addEventListener('keyup', handleKeyUp)
