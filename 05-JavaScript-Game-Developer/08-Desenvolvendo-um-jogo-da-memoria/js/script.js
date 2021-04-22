cd9d9c426df692a2c0dd83052ddbe9da13e64b34
Jeferson Melo <jefsdemelo@gmail.com>
2021-04-22 17:22:56 -0300

    ocultação automática do card; se iguais não ocultar

--- 05-JavaScript-Game-Developer/08-Desenvolvendo-um-jogo-da-memoria/js/script.js
+++ 05-JavaScript-Game-Developer/08-Desenvolvendo-um-jogo-da-memoria/js/script.js
@@ -1,10 +1,45 @@
 const cards = document.querySelectorAll('.card');
+var hasFlippedCard = false;
+var firstCard, secoundCard;
+var lockBoard = false;
 
 function flipCard() {
+    if (lockBoard) {
+        return;
+    }
     this.classList.toggle('flip');
+    if (!hasFlippedCard) {
+        hasFlippedCard = true;
+        firstCard = this;
+        return;
+    }
+
+    secoundCard = this;
+    hasFlippedCard = false;
+    checkForMath();
+}
+
+function checkForMath() {
+    if (firstCard.dataset.card === secoundCard.dataset.card) {
+        disableCards();
+        return;
+    }
+    unflipCard();
+}
+
+function disableCards() {
+    firstCard.removeEventListener('click', firstCard);
+    secoundCard.removeEventListener('click', firstCard);
+
+}
+
+function unflipCard() {
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secoundCard.classList.remove('flip');
+    }, 1500);
 }
 
 cards.forEach((card) => {
     card.addEventListener('click', flipCard)
 });
-
