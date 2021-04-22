a1b3b2319df1de12594a3929513cd1276310ec72
Jeferson Melo <jefsdemelo@gmail.com>
2021-04-22 17:03:04 -0300

    commit mecanica de revelação do card

---
+++ 05-JavaScript-Game-Developer/08-Desenvolvendo-um-jogo-da-memoria/js/script.js
@@ -0,0 +1,10 @@
+const cards = document.querySelectorAll('.card');
+
+function flipCard() {
+    this.classList.toggle('flip');
+}
+
+cards.forEach((card) => {
+    card.addEventListener('click', flipCard)
+});
+
