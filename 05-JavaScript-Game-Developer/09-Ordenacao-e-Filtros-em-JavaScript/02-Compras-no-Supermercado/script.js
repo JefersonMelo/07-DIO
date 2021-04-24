1db316a001ead07e169132d8c18bd1967be1215a
Jeferson Melo <jefsdemelo@gmail.com>
2021-04-24 14:39:19 -0300

    commit final, solução aceita

--- 05-JavaScript-Game-Developer/09-Ordenacao-e-Filtros-em-JavaScript/02-Compras-no-Supermercado/script.js
+++ 05-JavaScript-Game-Developer/09-Ordenacao-e-Filtros-em-JavaScript/02-Compras-no-Supermercado/script.js
@@ -1,10 +1,13 @@
 // a função gets é implementada dentro do sistema para ler as entradas(inputs) dos dados
-let totalItems = gets();
-
-for (var i = 0; i < totalItems; i++) {
+let totalItens = parseInt(gets());
+/*
+https://medium.com/trainingcenter/javascript-removendo-valores-duplicados-de-um-array-36e30e3d44c2
+https://pt.stackoverflow.com/questions/16483/remover-elementos-repetido-dentro-de-um-array-em-javascript
+*/
+for (var i = 0; i < totalItens; i++) {
   let itens = gets();
   let itensOrdenados = itens.split(" ").sort();
-  let itensUnicos = itensOrdenados; // Implemente aqui a lógica para ter os itens unicos
+  let itensUnicos = [...new Set(itensOrdenados)]; // Implemente aqui a lógica para ter os itens unicos
   let resposta = [...itensUnicos].join(" ");
   console.log(resposta);
 }
