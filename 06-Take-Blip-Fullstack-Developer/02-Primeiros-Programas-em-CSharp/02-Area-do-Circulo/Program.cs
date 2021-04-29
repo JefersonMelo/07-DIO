3f076fa3e11c6892faa1e997ef90095febd846f6
Jeferson Melo <jefsdemelo@gmail.com>
2021-04-29 17:50:02 -0300

    commit final

--- 06-Take-Blip-Fullstack-Developer/02-Primeiros-Programas-em-CSharp/02-Area-do-Circulo/Program.cs
+++ 06-Take-Blip-Fullstack-Developer/02-Primeiros-Programas-em-CSharp/02-Area-do-Circulo/Program.cs
@@ -6,7 +6,15 @@ namespace Area_do_Circulo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double pi = 3.14159, r;
+            double a;
+
+            r = Convert.ToDouble(Console.ReadLine());
+
+            a = pi * ( r * r );
+
+            Console.WriteLine("A=" + a.ToString("F4"));
         }
+
     }
 }
