9a14e6f922eafc0661b29fcd5cb5e071410346fe
Jeferson Melo <jefsdemelo@gmail.com>
2021-04-29 17:40:59 -0300

    commit inicial

---
+++ 06-Take-Blip-Fullstack-Developer/02-Primeiros-Programas-em-CSharp/Area-do-Circulo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Area_do_Circulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
