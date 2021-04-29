bee919b72fa47c541c651394fde256bcc5e90b85
Jeferson Melo <jefsdemelo@gmail.com>
2021-04-29 16:03:31 -0300

    commit inicial

---
+++ 06-Take-Blip-Fullstack-Developer/App_Cadastro_De_Series/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace App_Cadastro_De_Series
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
