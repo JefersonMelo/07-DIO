9740a96a47db2df30a90ac407f9ac19603267b8c
Jeferson Melo <jefsdemelo@gmail.com>
2021-04-29 17:53:39 -0300

    commit final

--- 06-Take-Blip-Fullstack-Developer/02-Primeiros-Programas-em-CSharp/03-Multiplo/Program.cs
+++ 06-Take-Blip-Fullstack-Developer/02-Primeiros-Programas-em-CSharp/03-Multiplo/Program.cs
@@ -6,7 +6,18 @@ namespace Multiplo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] valores = Console.ReadLine().Split(' ');
+            int a = int.Parse(valores[0]);
+            int b = int.Parse(valores[1]);
+
+            if ( a % b == 0 || b % a == 0 )
+            {
+                Console.WriteLine("Sao Multiplos");
+            }
+            else
+            {
+                Console.WriteLine("Nao sao Multiplos");
+            }
         }
     }
 }
