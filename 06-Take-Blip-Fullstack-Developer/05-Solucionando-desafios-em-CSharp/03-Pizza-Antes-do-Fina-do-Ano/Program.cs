1b9c8035b41dda3d31df718eb5715a3cdcfd73d9
Jeferson Melo <jefsdemelo@gmail.com>
2021-04-30 17:50:32 -0300

    commit inicial

---
+++ 06-Take-Blip-Fullstack-Developer/05-Solucionando-desafios-em-CSharp/03-Pizza-Antes-do-Fina-do-Ano/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+//continue a solução
+
+public class pizzaria
+{
+    public static void Main()
+    {
+        string[] line = Console.ReadLine().Split(" ");
+        int totalDePessoas = int.Parse(line[0]);
+        int totalDeDatas =      ;
+
+        string dataConsiderada = " ";
+
+        for (int i = 0; i < totalDeDatas; i++)
+        {
+            int totalDePessoasQuePodemComparecer =    ;
+
+            string[] entradaDataConfirmacao = Console.ReadLine().Split(" ");
+            dataConsiderada = ;
+
+            for (int j =  ; j <   ; j++)
+            {
+
+            }
+
+            if (totalDePessoasQuePodemComparecer ==    )
+            {
+                Console.WriteLine(dataConsiderada);
+                break;
+            }
+            else if (i + 1 >=       )
+            {
+                Console.WriteLine("Pizza antes de FdA");
+            }
+        }
+    }
+}
