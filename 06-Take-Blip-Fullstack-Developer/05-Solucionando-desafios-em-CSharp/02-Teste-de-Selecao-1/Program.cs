45405c4e8fffffd9b5642162dd569b3e7e35e374
Jeferson Melo <jefsdemelo@gmail.com>
2021-04-30 17:41:48 -0300

    commit incial

---
+++ 06-Take-Blip-Fullstack-Developer/05-Solucionando-desafios-em-CSharp/02-Teste-de-Selecao-1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _02_Teste_de_Selecao_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
