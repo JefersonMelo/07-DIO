fbf511762d12e8cbfec854abf2386f1a65c4114f
Jeferson Melo <jefsdemelo@gmail.com>
2021-04-30 17:43:30 -0300

    commit final

--- 06-Take-Blip-Fullstack-Developer/05-Solucionando-desafios-em-CSharp/01-Bazinga/Program.cs
+++ 06-Take-Blip-Fullstack-Developer/05-Solucionando-desafios-em-CSharp/01-Bazinga/Program.cs
@@ -8,26 +8,26 @@ namespace Bazinga
         {
             int qtdTeste = int.Parse(Console.ReadLine());
             string v1, v2;
-            for ( int i = 1; i <= qtdTeste; i++ ) //insira a variavel correta
+            for (int i = 1; i <= qtdTeste; i++) //insira a variavel correta
             {
                 string[] valores = Console.ReadLine().Split();
                 v1 = valores[0];
                 v2 = valores[1];
                 //complete a solução
-                if ( ( v1 == "tesoura" && v2 == "papel" ) //1. a tesoura corta o papel;
-                    || ( v1 == "papel" && v2 == "pedra" ) //2. o papel embrulha a pedra;
-                    || ( v1 == "pedra" && v2 == "lagarto" )   //3. a pedra esmaga o lagarto;
-                    || ( v1 == "lagarto" && v2 == "Spock" )  //4. o lagarto envenena Spock;
-                    || ( v1 == "Spock" && v2 == "tesoura" ) //5. Spock destrói a tesoura;
-                    || ( v1 == "tesoura" && v2 == "lagarto" ) //6.a tesoura decapita o lagarto;
-                    || ( v1 == "lagarto" && v2 == "papel" ) //7. o lagarto come o papel;
-                    || ( v1 == "papel" && v2 == "Spock" ) //8. o papel contesta Spock;
-                    || ( v1 == "Spock" && v2 == "pedra" ) //9. Spock vaporiza a pedra;
-                    || ( v1 == "pedra" && v2 == "tesoura" ) ) //10. a pedra quebra a tesoura.
+                if ((v1 == "tesoura" && v2 == "papel") //1. a tesoura corta o papel;
+                    || (v1 == "papel" && v2 == "pedra") //2. o papel embrulha a pedra;
+                    || (v1 == "pedra" && v2 == "lagarto")   //3. a pedra esmaga o lagarto;
+                    || (v1 == "lagarto" && v2 == "Spock")  //4. o lagarto envenena Spock;
+                    || (v1 == "Spock" && v2 == "tesoura") //5. Spock destrói a tesoura;
+                    || (v1 == "tesoura" && v2 == "lagarto") //6.a tesoura decapita o lagarto;
+                    || (v1 == "lagarto" && v2 == "papel") //7. o lagarto come o papel;
+                    || (v1 == "papel" && v2 == "Spock") //8. o papel contesta Spock;
+                    || (v1 == "Spock" && v2 == "pedra") //9. Spock vaporiza a pedra;
+                    || (v1 == "pedra" && v2 == "tesoura")) //10. a pedra quebra a tesoura.
                     Console.WriteLine("Caso #{0}: Bazinga!", i);
 
                 //complete a solucao
-                else if ( v1.ToString() == v2.ToString() )
+                else if (v1.ToString() == v2.ToString())
                     Console.WriteLine("Caso #{0}: De novo!", i);
                 else
                     Console.WriteLine("Caso #{0}: Raj trapaceou!", i);
--- 06-Take-Blip-Fullstack-Developer/05-Solucionando-desafios-em-CSharp/02-Teste-de-Selecao-1/Program.cs
+++ 06-Take-Blip-Fullstack-Developer/05-Solucionando-desafios-em-CSharp/02-Teste-de-Selecao-1/Program.cs
@@ -6,7 +6,21 @@ namespace _02_Teste_de_Selecao_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            int a, b, c, d;
+            string[] s = Console.ReadLine().Split(' ');
+            a = int.Parse(s[0]);
+            b = int.Parse(s[1]);
+            c = int.Parse(s[2]);
+            d = int.Parse(s[3]);
+            if (b > c && d > a && (c + d) > (a + b) && a % 2 == 0 && c > 0 && d > 0)
+            {
+                Console.WriteLine("Valores aceitos");
+            }
+            else
+            {
+                Console.WriteLine("Valores nao aceitos");
+            }
         }
     }
 }
