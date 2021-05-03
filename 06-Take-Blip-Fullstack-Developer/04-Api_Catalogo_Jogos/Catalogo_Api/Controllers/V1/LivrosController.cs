a371cab9167727ecfaeb06ea236b2c4114b8fc7e
Jeferson Melo <jefsdemelo@gmail.com>
2021-05-03 11:43:50 -0300

    commit implemetação dps imput e vew model

--- 06-Take-Blip-Fullstack-Developer/04-Api_Catalogo_Jogos/Catalogo_Api/Controllers/V1/LivrosController.cs
+++ 06-Take-Blip-Fullstack-Developer/04-Api_Catalogo_Jogos/Catalogo_Api/Controllers/V1/LivrosController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Catalogo_Api.ImputModel;
+using Catalogo_Api.ViewModel;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -12,49 +14,49 @@ namespace Catalogo_Api.Controllers.V1
     public class LivrosController : ControllerBase
     {
         [HttpGet]
-        public async Task<ActionResult<object>> Obter( )
+        public async Task<ActionResult<LivroViewModel>> Obter( )
         {
             return Ok();
         }
 
         [HttpGet("{idLivro:guid}")]
-        public async Task<ActionResult<object>> Obter(Guid idLivro)
+        public async Task<ActionResult<LivroViewModel>> Obter(Guid idLivro)
         {
             return Ok();
         }
 
         [HttpPost]
-        public async Task<ActionResult<object>> Inserir(object livro)
+        public async Task<ActionResult<LivroViewModel>> Inserir(LivroImputModel livro)
         {
             return Ok();
         }
 
         [HttpPut("{idLivro:guid}")]
-        public async Task<ActionResult<object>> Atualizar(Guid idLivro, object livro)
+        public async Task<ActionResult> Atualizar(Guid idLivro, LivroImputModel livro)
         {
             return Ok();
         }
 
-        [HttpPatch("{idLivro:guid}/sinopse/{sinopse:string}")]
-        public async Task<ActionResult<object>> Atualizar(Guid idLivro, string sinopse)
+        [HttpPatch("{idLivro:guid}/prefacio/{prefacio:string}")]
+        public async Task<ActionResult> Atualizar(Guid idLivro, string prefacio)
         {
             return Ok();
         }
 
         [HttpPatch("{idLivro:guid}/classificacao/{classificacao:int}")]
-        public async Task<ActionResult<object>> Atualizar(Guid idLivro, int classificacao)
+        public async Task<ActionResult> Atualizar(Guid idLivro, int classificacao)
         {
             return Ok();
         }
 
         [HttpPatch("{idLivro:guid}/preco/{preco:double}")]
-        public async Task<ActionResult<object>> Atualizar(Guid idLivro, double preco)
+        public async Task<ActionResult> Atualizar(Guid idLivro, double preco)
         {
             return Ok();
         }
 
         [HttpDelete("{idLivro:guid}")]
-        public async Task<ActionResult<object>> Apagar(Guid idLivro)
+        public async Task<ActionResult> Apagar(Guid idLivro)
         {
             return Ok();
         }
--- 06-Take-Blip-Fullstack-Developer/04-Api_Catalogo_Jogos/Catalogo_Api/ViewModel/LivroViewModel.cs
+++ 06-Take-Blip-Fullstack-Developer/04-Api_Catalogo_Jogos/Catalogo_Api/ViewModel/LivroViewModel.cs
@@ -5,12 +5,13 @@ using System.Threading.Tasks;
 
 namespace Catalogo_Api.ViewModel
 {
-    public class TrailerViewModel
+    public class LivroViewModel
     {
         public Guid Id { get; set; }
         public string Titulo { get; set; }
+        public string Autor { get; set; }
         public string Editora { get; set; }
-        public string Sinopse { get; set; }
+        public string Prefacio { get; set; }
         public int Classificacao { get; set; }
         public double Preco { get; set; }
     }
