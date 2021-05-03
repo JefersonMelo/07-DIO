eceba5f1029cf21e2b0f5441fa32879d9cf00658
Jeferson Melo <jefsdemelo@gmail.com>
2021-05-03 10:35:52 -0300

    commit mudança do projeto de trailers para catalogo de livros

---
+++ 06-Take-Blip-Fullstack-Developer/04-Api_Catalogo_Jogos/Catalogo_Api/Controllers/V1/LivrosController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalogo_Api.Controllers.V1
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LivrosController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<object>> Obter( )
+        {
+            return Ok();
+        }
+
+        [HttpGet("{idLivro:guid}")]
+        public async Task<ActionResult<object>> Obter(Guid idLivro)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<object>> Inserir(object livro)
+        {
+            return Ok();
+        }
+
+        [HttpPut("{idLivro:guid}")]
+        public async Task<ActionResult<object>> Atualizar(Guid idLivro, object livro)
+        {
+            return Ok();
+        }
+
+        [HttpPatch("{idLivro:guid}/sinopse/{sinopse:string}")]
+        public async Task<ActionResult<object>> Atualizar(Guid idLivro, string sinopse)
+        {
+            return Ok();
+        }
+
+        [HttpPatch("{idLivro:guid}/classificacao/{classificacao:int}")]
+        public async Task<ActionResult<object>> Atualizar(Guid idLivro, int classificacao)
+        {
+            return Ok();
+        }
+
+        [HttpPatch("{idLivro:guid}/preco/{preco:double}")]
+        public async Task<ActionResult<object>> Atualizar(Guid idLivro, double preco)
+        {
+            return Ok();
+        }
+
+        [HttpDelete("{idLivro:guid}")]
+        public async Task<ActionResult<object>> Apagar(Guid idLivro)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ 06-Take-Blip-Fullstack-Developer/04-Api_Catalogo_Jogos/Catalogo_Api/ViewModel/TrailerViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalogo_Api.ViewModel
+{
+    public class TrailerViewModel
+    {
+        public Guid Id { get; set; }
+        public string Titulo { get; set; }
+        public string Editora { get; set; }
+        public string Sinopse { get; set; }
+        public int Classificacao { get; set; }
+        public double Preco { get; set; }
+    }
+}
