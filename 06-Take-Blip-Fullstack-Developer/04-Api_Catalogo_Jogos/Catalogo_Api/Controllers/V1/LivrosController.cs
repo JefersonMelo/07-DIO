ce509d39cc4609b10faa1336d8e64dcec8ceb135
Jeferson Melo <jefsdemelo@gmail.com>
2021-05-04 15:53:29 -0300

    commit livrorepositoty, faltando a conexão com DB

--- 06-Take-Blip-Fullstack-Developer/04-Api_Catalogo_Jogos/Catalogo_Api/Controllers/V1/LivrosController.cs
+++ 06-Take-Blip-Fullstack-Developer/04-Api_Catalogo_Jogos/Catalogo_Api/Controllers/V1/LivrosController.cs
@@ -23,6 +23,16 @@ namespace Catalogo_Api.Controllers.V1
             _livroService = livroService;
         }
 
+        /// <summary>
+        /// Buscar todos os livros de forma paginada
+        /// </summary>
+        /// <remarks>
+        /// Não é possível retornar os livros sem paginação
+        /// </remarks>
+        /// <param name="pagina">Indica qual página está sendo consultada. Mínimo 1</param>
+        /// <param name="quantidade">Indica a quantidade de reistros por página. Mínimo 1 e máximo 50</param>
+        /// <response code="200">Retorna a lista de livros</response>
+        /// <response code="204">Caso não haja livros</response>  
         [HttpGet]
         public async Task<ActionResult<IEnumerable<LivroViewModel>>> Obter([FromQuery, Range(1, int.MaxValue)] int pagina = 1, [FromQuery, Range(1, 50)] int quanridade = 5)
         {
@@ -34,6 +44,12 @@ namespace Catalogo_Api.Controllers.V1
             return Ok(livros);
         }
 
+        /// <summary>
+        /// Buscar um livro pelo seu Id
+        /// </summary>
+        /// <param name="idLivro">Id do livro buscado</param>
+        /// <response code="200">Retorna o livro filtrado</response>
+        /// <response code="204">Caso não haja livro com este id</response>   
         [HttpGet("{idLivro:guid}")]
         public async Task<ActionResult<LivroViewModel>> Obter([FromRoute] Guid idLivro)
         {
@@ -45,20 +61,33 @@ namespace Catalogo_Api.Controllers.V1
             return Ok(livro);
         }
 
+        /// <summary>
+        /// Inserir um livro no catálogo
+        /// </summary>
+        /// <param name="livroInputModel">Dados do livro a ser inserido</param>
+        /// <response code="200">Cao o livro seja inserido com sucesso</response>
+        /// <response code="422">Caso já exista um livro com mesmo nome para a mesma produtora</response>   
         [HttpPost]
         public async Task<ActionResult<LivroViewModel>> Inserir([FromBody] LivroImputModel livroImputModel)
         {
             try
             {
-                var jogo = await _livroService.Inserir(livroImputModel);
-                return Ok(jogo);
+                var livro = await _livroService.Inserir(livroImputModel);
+                return Ok(livro);
             }
-            catch ( LivroJaCadastradoException ex )
+            catch ( LivroJaCadastradoException )
             {
                 return UnprocessableEntity("Já Existe um Livro com esse Título desta editora");
-            }            
+            }
         }
 
+        /// <summary>
+        /// Atualizar um livro no catálogo
+        /// </summary>
+        /// /// <param name="idLivro">Id do livro a ser atualizado</param>
+        /// <param name="livroInputModel">Novos dados para atualizar o livro indicado</param>
+        /// <response code="200">Cao o livro seja atualizado com sucesso</response>
+        /// <response code="404">Caso não exista um livro com este Id</response>   
         [HttpPut("{idLivro:guid}")]
         public async Task<ActionResult> Atualizar([FromRoute] Guid idLivro, [FromBody] LivroImputModel livroImputModel)
         {
@@ -67,12 +96,19 @@ namespace Catalogo_Api.Controllers.V1
                 await _livroService.Atualizar(idLivro, livroImputModel);
                 return Ok();
             }
-            catch ( LivroNaoCadastradoException ex )
+            catch ( LivroNaoCadastradoException )
             {
                 return NotFound("Este livro não existe!");
             }
         }
 
+        /// <summary>
+        /// Atualizar o prefácio de um livro
+        /// </summary>
+        /// /// <param name="idLivro">Id do livro a ser atualizado</param>
+        /// <param name="prefacio">Novo prefácio do livro</param>
+        /// <response code="200">Cao o prefácio seja atualizado com sucesso</response>
+        /// <response code="404">Caso não exista um livro com este Id</response>   
         [HttpPatch("{idLivro:guid}/prefacio/{prefacio:string}")]
         public async Task<ActionResult> Atualizar([FromRoute] Guid idLivro, [FromRoute] string prefacio)
         {
@@ -81,12 +117,19 @@ namespace Catalogo_Api.Controllers.V1
                 await _livroService.Atualizar(idLivro, prefacio);
                 return Ok();
             }
-            catch ( LivroNaoCadastradoException ex )
+            catch ( LivroNaoCadastradoException )
             {
                 return NotFound("Este livro não existe!");
             }
         }
 
+        /// <summary>
+        /// Atualizar o classificação de um livro
+        /// </summary>
+        /// /// <param name="idLivro">Id do livro a ser atualizado</param>
+        /// <param name="classificacao">Nova classificação do livro</param>
+        /// <response code="200">Cao a classificação seja atualizado com sucesso</response>
+        /// <response code="404">Caso não exista um livro com este Id</response>   
         [HttpPatch("{idLivro:guid}/classificacao/{classificacao:int}")]
         public async Task<ActionResult> Atualizar([FromRoute] Guid idLivro, [FromRoute] int classificacao)
         {
@@ -95,12 +138,19 @@ namespace Catalogo_Api.Controllers.V1
                 await _livroService.Atualizar(idLivro, classificacao);
                 return Ok();
             }
-            catch ( LivroNaoCadastradoException ex )
+            catch ( LivroNaoCadastradoException )
             {
                 return NotFound("Este livro não existe!");
             }
         }
 
+        /// <summary>
+        /// Atualizar o preço de um livro
+        /// </summary>
+        /// /// <param name="idLivro">Id do livro a ser atualizado</param>
+        /// <param name="preco">Novo preço do livro</param>
+        /// <response code="200">Cao o preço seja atualizado com sucesso</response>
+        /// <response code="404">Caso não exista um livro com este Id</response>   
         [HttpPatch("{idLivro:guid}/preco/{preco:double}")]
         public async Task<ActionResult> Atualizar([FromRoute] Guid idLivro, [FromRoute] double preco)
         {
@@ -109,12 +159,18 @@ namespace Catalogo_Api.Controllers.V1
                 await _livroService.Atualizar(idLivro, preco);
                 return Ok();
             }
-            catch ( LivroNaoCadastradoException ex )
+            catch ( LivroNaoCadastradoException )
             {
                 return NotFound("Este livro não existe!");
             }
         }
 
+        /// <summary>
+        /// Excluir um livro
+        /// </summary>
+        /// /// <param name="idLivro">Id do livro a ser excluído</param>
+        /// <response code="200">Cao o preço seja atualizado com sucesso</response>
+        /// <response code="404">Caso não exista um livro com este Id</response>  
         [HttpDelete("{idLivro:guid}")]
         public async Task<ActionResult> Apagar([FromRoute] Guid idLivro)
         {
@@ -123,11 +179,10 @@ namespace Catalogo_Api.Controllers.V1
                 await _livroService.Remover(idLivro);
                 return Ok();
             }
-            catch ( Exception )
+            catch ( LivroNaoCadastradoException )
             {
-
-                throw;
-            }            
+                return NotFound("Este Livro Não Existe");
+            }
         }
     }
 }
--- 06-Take-Blip-Fullstack-Developer/04-Api_Catalogo_Jogos/Catalogo_Api/Repositories/LivroRepository.cs
+++ 06-Take-Blip-Fullstack-Developer/04-Api_Catalogo_Jogos/Catalogo_Api/Repositories/LivroRepository.cs
@@ -8,39 +8,70 @@ namespace Catalogo_Api.Repositories
 {
     public class LivroRepository : ILivroRepositoriy
     {
-        public Task Atualizar(Livro livro)
+        private Dictionary<Guid, Livro> livros = new Dictionary<Guid, Livro>()
         {
-            throw new NotImplementedException();
-        }
-
-        public void Dispose( )
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<Livro> Inserir(Livro livro)
-        {
-            throw new NotImplementedException();
-        }
+            {Guid.Parse("0ca314a5-9282-45d8-92c3-2985f2a9fd04"), new Livro{ Id = Guid.Parse("0ca314a5-9282-45d8-92c3-2985f2a9fd04"), Titulo = "Fifa 21", Editora = "EA", Preco = 200, Autor = "AAAA", Classificacao = 12, Prefacio = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"} },
+            {Guid.Parse("eb909ced-1862-4789-8641-1bba36c23db3"), new Livro{ Id = Guid.Parse("eb909ced-1862-4789-8641-1bba36c23db3"), Titulo = "Fifa 20", Editora = "EA", Preco = 190, Autor = "BBBB", Classificacao = 18, Prefacio = "BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB"} },
+            {Guid.Parse("5e99c84a-108b-4dfa-ab7e-d8c55957a7ec"), new Livro{ Id = Guid.Parse("5e99c84a-108b-4dfa-ab7e-d8c55957a7ec"), Titulo = "Fifa 19", Editora = "EA", Preco = 180, Autor = "CCCC", Classificacao = 10, Prefacio = "CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC"} },
+            {Guid.Parse("da033439-f352-4539-879f-515759312d53"), new Livro{ Id = Guid.Parse("da033439-f352-4539-879f-515759312d53"), Titulo = "Fifa 18", Editora = "EA", Preco = 170, Autor = "DDDD", Classificacao = 12, Prefacio = "DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD"} },
+            {Guid.Parse("92576bd2-388e-4f5d-96c1-8bfda6c5a268"), new Livro{ Id = Guid.Parse("92576bd2-388e-4f5d-96c1-8bfda6c5a268"), Titulo = "Street Fighter V", Editora = "Capcom", Preco = 80, Autor = "EEEE", Classificacao = 12, Prefacio = "EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE"} },
+            {Guid.Parse("c3c9b5da-6a45-4de1-b28b-491cbf83b589"), new Livro{ Id = Guid.Parse("c3c9b5da-6a45-4de1-b28b-491cbf83b589"), Titulo = "Grand Theft Auto V", Editora = "Rockstar", Preco = 190, Autor = "FFFF", Classificacao = 12, Prefacio = "FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF"} }
+        };
 
         public Task<List<Livro>> Obter(int pagina, int quantidade)
         {
-            throw new NotImplementedException();
+            return Task.FromResult(livros.Values.Skip(( pagina - 1 ) * quantidade).Take(quantidade).ToList());
         }
 
-        public Task<List<Livro>> Obter(string autor, string editora)
+        public Task<List<Livro>> Obter(string autor, string titulo)
         {
-            throw new NotImplementedException();
+            return Task.FromResult(livros.Values.Where(livro => livro.Autor.Equals(autor) && livro.Titulo.Equals(titulo)).ToList());
+        }
+
+        public Task<List<Livro>> ObterSemLambda(string autor, string titulo)
+        {
+            var retorno = new List<Livro>();
+
+            foreach ( var livro in livros.Values )
+            {
+                if ( livro.Autor.Equals(autor) && livro.Titulo.Equals(titulo) )
+                {
+                    retorno.Add(livro);
+                }
+            }
+            return Task.FromResult(retorno);
         }
 
         public Task<Livro> Obter(Guid id)
         {
-            throw new NotImplementedException();
+            if ( !livros.ContainsKey(id) )
+            {
+                return Task.FromResult<Livro>(null);
+            }
+            return Task.FromResult(livros[id]);
+        }
+
+        public Task<Livro> Inserir(Livro livro)
+        {
+            livros.Add(livro.Id, livro);
+            return ( Task<Livro> )Task.CompletedTask;
+        }
+
+        public Task Atualizar(Livro livro)
+        {
+            livros[livro.Id] = livro;
+            return Task.CompletedTask;
         }
 
         public Task Remover(Guid id)
         {
-            throw new NotImplementedException();
+            livros.Remove(id);
+            return Task.CompletedTask;
+        }
+
+        public void Dispose( )
+        {
+            //Fechar conexão com o banco
         }
     }
 }
