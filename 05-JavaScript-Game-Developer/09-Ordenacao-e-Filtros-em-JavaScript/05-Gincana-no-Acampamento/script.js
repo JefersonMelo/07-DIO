f2aa4bec622fbc39133c4c68e1d8166b94d7d704
Jeferson Melo <jefsdemelo@gmail.com>
2021-05-13 13:50:06 -0300

    commit inicial

--- 05-JavaScript-Game-Developer/09-Ordenacao-e-Filtros-em-JavaScript/05-Gincana-no-Acampamento/script.js
+++ 05-JavaScript-Game-Developer/09-Ordenacao-e-Filtros-em-JavaScript/05-Gincana-no-Acampamento/script.js
@@ -1,2 +1,27 @@
 // a função gets é implementada dentro do sistema para ler as entradas(inputs) dos dados
-let totalDeCasos = parseInt(gets());
+quantAlunos = gets();
+
+while (quantAlunos > 0) {
+    listaAlunos = [];
+    for (i = 1; i <= quantAlunos; i++) {
+        arrayAux = gets().split(' ');
+        listaAlunos.push({
+            nome: arrayAux[0],
+            valor: parseInt(arrayAux[1])
+        });
+    }
+    contadorValorFicha = 0;
+    valorFicha = listaAlunos[0].valor;
+    while ((arrQuantAlunoPorGrupo = listaAlunos.length) > 1) {
+        (valorFicha % 2 == 1) ?
+        contadorValorFicha = (contadorValorFicha + valorFicha) % arrQuantAlunoPorGrupo:
+            contadorValorFicha = (contadorValorFicha - (valorFicha % arrQuantAlunoPorGrupo)) % arrQuantAlunoPorGrupo;
+        if (contadorValorFicha < 0) contadorValorFicha = arrQuantAlunoPorGrupo + contadorValorFicha;
+        valorFicha = listaAlunos[contadorValorFicha].valor;
+        listaAlunos.splice(contadorValorFicha, 1);
+        if (valorFicha % 2 == 1) contadorValorFicha--;
+        arrQuantAlunoPorGrupo--;
+    }
+    console.log("Vencedor(a): " + listaAlunos[0].nome);
+    quantAlunos = gets();
+}
