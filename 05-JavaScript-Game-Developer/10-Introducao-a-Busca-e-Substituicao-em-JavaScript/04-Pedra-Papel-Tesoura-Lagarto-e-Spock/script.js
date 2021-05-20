5ea49c107ca56e272ff35403eb7f04d0ffd7dbbd
Jeferson Melo <jefsdemelo@gmail.com>
2021-05-20 12:07:29 -0300

    commit com solução Bazinga! aceito

--- 05-JavaScript-Game-Developer/10-Introducao-a-Busca-e-Substituicao-em-JavaScript/04-Pedra-Papel-Tesoura-Lagarto-e-Spock/script.js
+++ 05-JavaScript-Game-Developer/10-Introducao-a-Busca-e-Substituicao-em-JavaScript/04-Pedra-Papel-Tesoura-Lagarto-e-Spock/script.js
@@ -1,17 +1,20 @@
-const input = require('fs').readFileSync('/dev/stdin', 'utf8');
+/*const input = require('fs').readFileSync('/dev/stdin', 'utf8');
 const lines = input.split('\n');
+https://www.devmedia.com.br/javascript-split-dividindo-separando-strings/39254
+https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/String/split
 
+*/
 let dicionarioDeRegras = {
-    tesoura: ['papel','lagarto'],
-    papel: ['pedra','spock'],
-    pedra: ['lagarto','tesoura'],
-    lagarto: ['spock','papel'],
-    spock: ['tesoura','pedra']
+    tesoura: ['papel', 'lagarto'],
+    papel: ['pedra', 'spock'],
+    pedra: ['lagarto', 'tesoura'],
+    lagarto: ['spock', 'papel'],
+    spock: ['tesoura', 'pedra']
 };
 
 //finalize a declaração das jogadoras
-let  = 'fernanda';
-let  = 'marcia';
+let gamer_1 = 'fernanda';
+let gamer_2 = 'marcia';
 
 let resultado;
 
@@ -19,13 +22,12 @@ let N = parseInt(gets());
 
 // continue a solução
 
-for(   ){
+for (let i = 0; i < N; i++) {
 
-let     = gets().split(' ');
-if(   ===  )
-resultado = 'empate';
-else
-resultado = (dicionarioDeRegras[     .includes(  ] ) ) ?   :    ;
-
-print(resultado);
+    let resposta = gets().split(' ');
+    if (resposta[0] === resposta[1])
+        resultado = 'empate';
+    else
+        resultado = (dicionarioDeRegras[resposta[0]].includes(resposta[1])) ? gamer_1: gamer_2;
+    print(resultado);
 }
