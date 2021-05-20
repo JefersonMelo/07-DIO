52f5e9d47142cb4d784bf98258a9b860a33b4a76
Jeferson Melo <jefsdemelo@gmail.com>
2021-05-20 10:53:42 -0300

    commit solução Encontre a maior substring aceito

--- 05-JavaScript-Game-Developer/10-Introducao-a-Busca-e-Substituicao-em-JavaScript/02-Encontre-a-maior-substring/script.js
+++ 05-JavaScript-Game-Developer/10-Introducao-a-Busca-e-Substituicao-em-JavaScript/02-Encontre-a-maior-substring/script.js
@@ -1,3 +1,30 @@
 const input = require('fs').readFileSync('/dev/stdin', 'utf8');
 const lines = input.split('\n');
 
+let string1 = gets();
+let string2 = gets();
+
+while (string1 !== "") {
+  if (string1.length >= string2.length) {
+    countChar(string2, string1);
+  } else {
+    countChar(string1, string2);
+  }
+  string1 = gets();
+  string2 = gets();
+}
+
+function countChar(menorString, maiorString) {
+  let count = 0;
+
+  for (let i = 0; i <= maiorString.length; i++) {
+    for (let j = 0; j < maiorString.length; j++) {
+      let subString = maiorString.substring(i, j);
+      
+      if (menorString.includes(subString) && subString.length > count) {
+        count = subString.length;
+      }
+    }
+  }
+  console.log(count);
+}
