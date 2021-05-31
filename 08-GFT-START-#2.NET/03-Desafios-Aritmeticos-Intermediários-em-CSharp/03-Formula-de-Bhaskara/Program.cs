e3684083b9a09cee2736ac4f5594191c22e581d0
Jeferson Melo <jefsdemelo@gmail.com>
2021-05-31 12:28:37 -0300

    commit final fórmula de bhaskara

--- 08-GFT-START-#2.NET/03-Desafios-Aritmeticos-Intermediários-em-CSharp/03-Formula-de-Bhaskara/Program.cs
+++ 08-GFT-START-#2.NET/03-Desafios-Aritmeticos-Intermediários-em-CSharp/03-Formula-de-Bhaskara/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace Formula_de_Bhaskara
 {
@@ -6,7 +7,23 @@ namespace Formula_de_Bhaskara
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double a, b, c, delta, x1, x2;
+            string[] s = Console.ReadLine().Split(' ');
+            a = double.Parse(s[0]);
+            b = double.Parse(s[1]);
+            c = double.Parse(s[2]);
+            delta = Math.Pow(b, 2) - 4 * a * c;
+            if(delta >= 0 && a != 0)
+            {
+                x1 = (-b + Math.Sqrt(delta)) / (2 * a);
+                x2 = (-b - Math.Sqrt(delta)) / (2 * a);
+                Console.WriteLine($"R1 = {x1.ToString("F5", CultureInfo.InvariantCulture)}");
+                Console.WriteLine($"R2 = {x2.ToString("F5", CultureInfo.InvariantCulture)}");
+            }
+            else
+            {
+                Console.WriteLine("Impossivel calcular");
+            }
         }
     }
 }
