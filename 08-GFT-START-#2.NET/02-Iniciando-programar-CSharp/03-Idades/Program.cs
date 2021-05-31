43c26d12346bb379bb7d9ac46b6e5658d698e176
Jeferson Melo <jefsdemelo@gmail.com>
2021-05-31 11:14:32 -0300

    commit conclusão Idades

--- 08-GFT-START-#2.NET/02-Iniciando-programar-CSharp/03-Idades/Program.cs
+++ 08-GFT-START-#2.NET/02-Iniciando-programar-CSharp/03-Idades/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace Idades
 {
@@ -6,7 +7,23 @@ namespace Idades
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = 1;
+            int cont = 0;
+            int soma = 0;
+
+            while(n > 0)
+            {
+                n = int.Parse(Console.ReadLine());
+
+                if(n > 0)
+                {
+                    soma += n;
+                    cont++;
+                }
+            }
+            double media = (double)soma / cont;
+
+            Console.WriteLine(media.ToString("F2", CultureInfo.InvariantCulture));
         }
     }
 }
