e2a2c51c9f63dddff15d36d9ff3b297a289743f6
Jeferson Melo <jefsdemelo@gmail.com>
2021-05-31 12:14:10 -0300

    commit inicial tipos de triângulo

---
+++ 08-GFT-START-#2.NET/03-Desafios-Aritmeticos-Intermediários-em-CSharp/02-Tipos-de-Triangulos/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tipos_de_Triangulos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
