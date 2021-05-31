78bf5a537d1fba9c4b86b1a869673ab35fbc4de9
Jeferson Melo <jefsdemelo@gmail.com>
2021-05-31 11:39:28 -0300

    commit conclusão Soma Simples

--- 08-GFT-START-#2.NET/02-Iniciando-programar-CSharp/02-Soma-Simples/Program.cs
+++ 08-GFT-START-#2.NET/02-Iniciando-programar-CSharp/02-Soma-Simples/Program.cs
@@ -6,7 +6,12 @@ namespace Soma_Simples
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            int soma = a + b;
+
+            Console.WriteLine("SOMA = {0}", soma);
         }
     }
 }
