d72b7dddafd04ba36c498d83dc4351b14014cc29
Jeferson Melo <jefsdemelo@gmail.com>
2021-05-31 12:06:21 -0300

    commit conclusão notas e moedas

--- 08-GFT-START-#2.NET/03-Desafios-Aritmeticos-Intermediários-em-CSharp/01-Notas-e-Moedas/Program.cs
+++ 08-GFT-START-#2.NET/03-Desafios-Aritmeticos-Intermediários-em-CSharp/01-Notas-e-Moedas/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace Notas_e_Moedas
 {
@@ -6,7 +7,37 @@ namespace Notas_e_Moedas
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int notas, moedas;
+            string[] v = Console.ReadLine().Split('.');
+            notas = int.Parse(v[0], CultureInfo.InvariantCulture);
+            moedas = int.Parse(v[1], CultureInfo.InvariantCulture);
+
+            Console.WriteLine("NOTAS:");
+            Console.WriteLine($"{notas / 100} nota(s) de R$ 100.00");//576
+            notas %= 100;//576 - 500 = 76
+            Console.WriteLine($"{notas / 50} nota(s) de R$ 50.00");//76  
+            notas %= 50;//76 - 50 = 26
+            Console.WriteLine($"{notas / 20} nota(s) de R$ 20.00");//26
+            notas %= 20;//26 - 20 = 6
+            Console.WriteLine($"{notas / 10} nota(s) de R$ 10.00");//6
+            notas %= 10;//6 - 10 = 0
+            Console.WriteLine($"{notas / 5} nota(s) de R$ 5.00");//6
+            notas %= 5;//6 - 5 = 1
+            Console.WriteLine($"{notas / 2} nota(s) de R$ 2.00");//1
+            //moedas
+            Console.WriteLine("MOEDAS:");
+            notas %= 2;
+            Console.WriteLine($"{notas} moeda(s) de R$ 1.00");
+            moedas %= 100;
+            Console.WriteLine($"{moedas / 50} moeda(s) de R$ 0.50");
+            moedas %= 50;
+            Console.WriteLine($"{moedas / 25} moeda(s) de R$ 0.25");
+            moedas %= 25;
+            Console.WriteLine($"{moedas / 10} moeda(s) de R$ 0.10");
+            moedas %= 10;
+            Console.WriteLine($"{moedas / 5} moeda(s) de R$ 0.05");
+            moedas %= 5;
+            Console.WriteLine($"{moedas} moeda(s) de R$ 0.01");
         }
     }
 }
--- 08-GFT-START-#2.NET/README.md
+++ 08-GFT-START-#2.NET/README.md
@@ -2,7 +2,7 @@
 
 Desenvolva seus primeiros passos e primeiros projetos na stack de .net C# com grandes experts de mercado e construa aplicações de alto nível com embasamento técnico de arquitetura em back-end.  
 
-## Iniciando a programar em C``#``
+## Iniciando a programar em ``C#``
 
 | ID  | Desafio                                                                                                                                                     | Status             |
 | --- | ----------------------------------------------------------------------------------------------------------------------------------------------------------- | ------------------ |
@@ -10,10 +10,10 @@ Desenvolva seus primeiros passos e primeiros projetos na stack de .net C# com gr
 | 2   | [Soma Simples](https://github.com/JefersonMelo/07-DIO/blob/master/08-GFT-START-%232.NET/02-Iniciando-programar-CSharp/02-Soma-Simples/Program.cs)           | :white_check_mark: |
 | 3   | [Idades](https://github.com/JefersonMelo/07-DIO/blob/master/08-GFT-START-%232.NET/02-Iniciando-programar-CSharp/03-Idades/Program.cs)                       | :white_check_mark: |
 
-## Desafios Aritméticos Intermediários em C``#``
+## Desafios Aritméticos Intermediários em ``C#``
 
 |ID|Desafio|Status|
 |-|-|-|
-|1|[Notas e Moedas]()|:white_check_mark:|
+|1|[Notas e Moedas](https://github.com/JefersonMelo/07-DIO/blob/master/08-GFT-START-%232.NET/03-Desafios-Aritmeticos-Intermedi%C3%A1rios-em-CSharp/01-Notas-e-Moedas/Program.cs)|:white_check_mark:|
 |2|[]()|:white_check_mark:|
 |3|[]()|:white_check_mark:|
