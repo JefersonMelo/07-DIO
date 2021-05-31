f29196ca814f21ac246aeec1215c374d249343b4
Jeferson Melo <jefsdemelo@gmail.com>
2021-05-31 11:32:06 -0300

    commit conclusão Dividindo X Por Y

--- 08-GFT-START-#2.NET/02-Iniciando-programar-CSharp/01-Dividindo-X-por-Y/Program.cs
+++ 08-GFT-START-#2.NET/02-Iniciando-programar-CSharp/01-Dividindo-X-por-Y/Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
+using System.Globalization;
 
-namespace Dividindo_X_por_Y
+namespace Dividindo_X_Por_Y
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double r;
+            int n, x, y;
+            n = int.Parse(Console.ReadLine());
+            for(int i = 0; i < n; i++)
+            {
+                string[] s = Console.ReadLine().Split(' ');
+                x = int.Parse(s[0], CultureInfo.InvariantCulture);
+                y = int.Parse(s[1], CultureInfo.InvariantCulture);
+
+                if(y == 0)
+                {
+                    Console.WriteLine("divisao impossivel");
+                }
+                else
+                {
+                    r = (double)x / y;
+                    Console.WriteLine(r.ToString("F1", CultureInfo.InvariantCulture));
+                }
+            }
         }
     }
 }
