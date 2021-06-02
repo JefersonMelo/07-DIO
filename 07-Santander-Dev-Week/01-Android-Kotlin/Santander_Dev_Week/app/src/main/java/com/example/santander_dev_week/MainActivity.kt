770cd73d7d35c386f62708912ff154e70005a49c
Jeferson Melo <jefsdemelo@gmail.com>
2021-06-02 13:44:46 -0300

    commit inicial: cores principais e icon-app-launcher definido

---
+++ 07-Santander-Dev-Week/01-Android-Kotlin/Santander_Dev_Week/app/src/main/java/com/example/santander_dev_week/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.santander_dev_week
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflate = menuInflater
+        inflate.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId){
+            R.id.idItem_01 -> {
+                Log.d("CLICK", "Click no Item 1")
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
