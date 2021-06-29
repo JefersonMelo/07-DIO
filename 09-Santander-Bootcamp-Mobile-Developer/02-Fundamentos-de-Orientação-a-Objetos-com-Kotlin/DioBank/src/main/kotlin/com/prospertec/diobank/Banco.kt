239c55b0322704ffa7171399ef0d3cd0c4a6d7db
Jeferson Melo <jefsdemelo@gmail.com>
2021-06-29 15:48:32 -0300

    data class Banco fun info

--- 09-Santander-Bootcamp-Mobile-Developer/02-Fundamentos-de-Orientação-a-Objetos-com-Kotlin/DioBank/src/main/kotlin/com/prospertec/diobank/Banco.kt
+++ 09-Santander-Bootcamp-Mobile-Developer/02-Fundamentos-de-Orientação-a-Objetos-com-Kotlin/DioBank/src/main/kotlin/com/prospertec/diobank/Banco.kt
@@ -1,3 +1,11 @@
 package com.prospertec.diobank
 
-data class Banco(val nome: String, val num: Int, val endereco: String)
+data class Banco(
+    val nome: String,
+    val num: Int,
+    val endereco: String
+) {
+    fun info() = "Nome: $nome\n " +
+            "Número: $num\n " +
+            "Endereço: $endereco"
+}
--- 09-Santander-Bootcamp-Mobile-Developer/02-Fundamentos-de-Orientação-a-Objetos-com-Kotlin/DioBank/src/main/kotlin/com/prospertec/diobank/teste/BancoTeste.kt
+++ 09-Santander-Bootcamp-Mobile-Developer/02-Fundamentos-de-Orientação-a-Objetos-com-Kotlin/DioBank/src/main/kotlin/com/prospertec/diobank/teste/BancoTeste.kt
@@ -1,5 +1,8 @@
 package com.prospertec.diobank.teste
 
-fun main() {
+import com.prospertec.diobank.Banco
 
+fun main() {
+    val bank = Banco(nome = "DioBank", num = 1, endereco = "web")
+    println(bank.info())
 }
