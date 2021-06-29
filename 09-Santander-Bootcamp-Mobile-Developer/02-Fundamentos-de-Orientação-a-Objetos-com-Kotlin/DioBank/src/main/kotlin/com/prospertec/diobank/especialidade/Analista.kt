99811088e6740fd9a35ab7b8e038e5e85858855c
Jeferson Melo <jefsdemelo@gmail.com>
2021-06-29 18:09:44 -0300

    class e teste Gerente

--- 09-Santander-Bootcamp-Mobile-Developer/02-Fundamentos-de-Orientação-a-Objetos-com-Kotlin/DioBank/src/main/kotlin/com/prospertec/diobank/especialidade/Analista.kt
+++ 09-Santander-Bootcamp-Mobile-Developer/02-Fundamentos-de-Orientação-a-Objetos-com-Kotlin/DioBank/src/main/kotlin/com/prospertec/diobank/especialidade/Analista.kt
@@ -3,5 +3,5 @@ package com.prospertec.diobank.especialidade
 import com.prospertec.diobank.Funcionario
 
 class Analista(nome: String, cpf: String, salario: Double) : Funcionario(nome, cpf, salario) {
-    override fun calcularAuxilio() = salario * 0.1
+    override fun calcularAuxilio(): Double = salario * 0.1
 }
---
+++ 09-Santander-Bootcamp-Mobile-Developer/02-Fundamentos-de-Orientação-a-Objetos-com-Kotlin/DioBank/src/main/kotlin/com/prospertec/diobank/teste/TesteGerente.kt
@@ -0,0 +1,11 @@
+package com.prospertec.diobank.teste
+
+import com.prospertec.diobank.especialidade.Gerente
+
+fun main() {
+    val f = Gerente(nome = "Jeff", cpf = "222.222.222-22", salario = 15000.0)
+
+    println(f)
+
+}
+
