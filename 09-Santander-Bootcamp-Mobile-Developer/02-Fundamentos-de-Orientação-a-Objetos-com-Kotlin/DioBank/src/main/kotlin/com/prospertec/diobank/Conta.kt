1a8dce018b750766942fcd2532a4375a55c12b93
Jeferson Melo <jefsdemelo@gmail.com>
2021-06-29 11:23:47 -0300

    classe conta e atributos: agencia, npúmero, saldo. funs deposito e saque

---
+++ 09-Santander-Bootcamp-Mobile-Developer/02-Fundamentos-de-Orientação-a-Objetos-com-Kotlin/DioBank/src/main/kotlin/com/prospertec/diobank/Conta.kt
@@ -0,0 +1,14 @@
+package com.prospertec.diobank
+
+import java.math.BigDecimal
+
+class Conta(val agencia: String, val numero: String, val saldo: BigDecimal) {
+
+    fun deposito(valor: BigDecimal){
+
+    }
+    fun saque(valor: BigDecimal){
+
+    }
+
+}
