5738ddd9a20eba8af6a47da653649a622d4d3a05
Jeferson Melo <jefsdemelo@gmail.com>
2021-07-01 10:49:54 -0300

    criação teste array e reordenarção

---
+++ 09-Santander-Bootcamp-Mobile-Developer/04-Aplicando-Conceitos-de-Coleções-Arrays-e-Listas/01-Collections/src/main/kotlin/com/prospertec/collections/Main.kt
@@ -0,0 +1,4 @@
+package com.prospertec.collections
+
+class main {
+}
---
+++ 09-Santander-Bootcamp-Mobile-Developer/04-Aplicando-Conceitos-de-Coleções-Arrays-e-Listas/01-Collections/src/main/kotlin/com/prospertec/collections/testes/TesteArrays.kt
@@ -0,0 +1,41 @@
+package com.prospertec.collections.testes
+
+fun main() {
+    //Array
+    val arr = IntArray(5)
+    var cont: Int = 0
+
+    //Preenchimento dinâmico
+    for (index in arr.indices) {
+        arr[index] = cont++
+    }
+
+    //1ª Forma de iterar
+    print("1ª: ")
+    for (n in arr) {
+        print("$n ")
+    }
+    println()
+
+    //2ª Forma de iterar
+    print("2ª: ")
+    arr.forEach {
+        print("$it ")
+    }
+    println()
+
+    //3ª Forma de iterar
+    print("3ª: ")
+    for (i in arr.indices) {
+        print("""${arr[i]} """)
+    }
+    println()
+
+    //Reordenação
+    arr.sortDescending()
+    print("Reorder: ")
+    arr.forEach {
+        print("$it ")
+    }
+
+}
