32418bfed558d8edcc7903039a5f2e415def3bd2
Jeferson Melo <jefsdemelo@gmail.com>
2021-07-01 18:56:09 -0300

    criar TesteSetMutable.kt

---
+++ 09-Santander-Bootcamp-Mobile-Developer/04-Aplicando-Conceitos-de-Coleções-Arrays-e-Listas/01-Collections/src/main/kotlin/com/prospertec/collections/testesMutable/TesteSetMutable.kt
@@ -0,0 +1,25 @@
+package com.prospertec.collections.testesMutable
+
+import com.prospertec.collections.Funcionario
+
+fun main() {
+    val f1 = Funcionario(nome = "F1", salario = 4500.0, tipoContato = "PJ")
+    val f2 = Funcionario(nome = "F2", salario = 3500.0, tipoContato = "PJ")
+    val f3 = Funcionario(nome = "F3", salario = 2500.0, tipoContato = "CLT")
+    val f4 = Funcionario(nome = "F4", salario = 1500.0, tipoContato = "CLT")
+
+    val fs = mutableSetOf(f1, f2)
+    fs.forEach { println(it) }
+
+    //Método adicionar
+    println("-----------------")
+    fs.add(f3)
+    fs.add(f3)
+    fs.add(f3)
+    fs.forEach { println(it) }
+
+    //Método remover
+    println("-----------------")
+    fs.remove(f1)
+    fs.forEach { println(it) }
+}
