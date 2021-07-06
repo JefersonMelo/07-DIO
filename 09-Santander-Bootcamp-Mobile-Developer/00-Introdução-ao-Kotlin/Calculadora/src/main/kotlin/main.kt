2d107b9a5ed5ea0f61cbd561f87d71666e9992e7
Jeferson Melo <jefsdemelo@gmail.com>
2021-07-06 16:25:55 -0300

    refatoração ifs para when

--- 09-Santander-Bootcamp-Mobile-Developer/00-Introdução-ao-Kotlin/Calculadora/src/main/kotlin/main.kt
+++ 09-Santander-Bootcamp-Mobile-Developer/00-Introdução-ao-Kotlin/Calculadora/src/main/kotlin/main.kt
@@ -2,69 +2,60 @@ import java.text.DecimalFormat
 
 fun main() {
 
-    do{
+    do {
         print("Valor A: ")
-        val a:Float = readLine()!!.toFloat()
+        val a: Float = readLine()!!.toFloat()
 
         println("Escolha Uma Operação: + - * / % ou S para sair")
         print("Opção: ")
-        val op:String = readLine().toString().uppercase()
+        val op: String = readLine().toString().uppercase()
 
         print("Valor B: ")
-        val b:Float = readLine()!!.toFloat()
+        val b: Float = readLine()!!.toFloat()
 
-        if (op!="S"){
+        if (op != "S") {
             print(resultado(op, a, b))
             print("Continuar S ou N: ")
-            val c:String = readLine().toString().uppercase()
-            if (c=="N")break
+            val c: String = readLine().toString().uppercase()
+            if (c == "N") break
         }
 
-    }while (op != "S")
+    } while (op != "S")
 
     println("***** Fim *****")
 
 }
 
-fun resultado(op:String, a: Float, b: Float):String{
+fun resultado(op: String, a: Float, b: Float): String {
     val dec = DecimalFormat("##.##")
     var r = 0f
 
-    if(op == "+"){
-        r = soma(a, b)
-    }
-
-    if(op == "-"){
-        r = subtracao(a, b)
-    }
-    if(op == "*"){
-        r = multiplicacao(a, b)
-    }
-    if(op == "/"){
-        r = divisao(a, b)
-    }
-    if(op == "%"){
-        r = restoDivisao(a, b)
+    when (op) {
+        "+" -> { r = soma(a, b) }
+        "-" -> { r = subtracao(a, b) }
+        "*" -> { r = multiplicacao(a, b) }
+        "/" -> { r = divisao(a, b) }
+        "%" -> { r = restoDivisao(a, b) }
     }
     return "Resultado: ${dec.format(r)}\n"
 }
 
-fun soma(a:Float, b:Float): Float{
+fun soma(a: Float, b: Float): Float {
     return a + b
 }
 
-fun subtracao(a:Float, b:Float): Float{
+fun subtracao(a: Float, b: Float): Float {
     return a - b
 }
 
-fun multiplicacao(a:Float, b:Float): Float{
+fun multiplicacao(a: Float, b: Float): Float {
     return a * b
 }
 
-fun divisao(a:Float, b:Float): Float{
+fun divisao(a: Float, b: Float): Float {
     return a / b
 }
 
-fun restoDivisao(a:Float, b:Float): Float{
+fun restoDivisao(a: Float, b: Float): Float {
     return a % b
 }
---
+++ 09-Santander-Bootcamp-Mobile-Developer/06-Projetando-o-Primeiro-Aplicativo-Android-Usando-Kotlin/README.md
@@ -0,0 +1,6 @@
+# App IMC
+
+## Estudar
+
+1. Theme.MaterialComponents.Light.BottomSheetDialog
+2. Light.BottomSheetDialog
