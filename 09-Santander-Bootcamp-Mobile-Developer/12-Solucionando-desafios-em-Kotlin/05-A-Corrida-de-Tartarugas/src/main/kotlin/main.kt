145b2e18edc586db85a7fcb037aa1f29c109932a
Jeferson Melo <jefsdemelo@gmail.com>
2021-07-08 15:39:31 -0300

    commit final Corrida de Tartarugas

--- 09-Santander-Bootcamp-Mobile-Developer/12-Solucionando-desafios-em-Kotlin/05-A-Corrida-de-Tartarugas/src/main/kotlin/main.kt
+++ 09-Santander-Bootcamp-Mobile-Developer/12-Solucionando-desafios-em-Kotlin/05-A-Corrida-de-Tartarugas/src/main/kotlin/main.kt
@@ -1,3 +1,28 @@
+import java.io.EOFException
+import java.lang.Exception
+
 fun main(args: Array<String>) {
-    println("Hello World!")
+
+    while (readLine().isNullOrEmpty()) {
+
+        try {
+
+            val l = readLine()?.toInt()
+            val v = readLine()!!.split(" ").run { map { it.toInt() }}
+            val orderAsc = v.toTypedArray()
+            orderAsc.sort()
+            when (orderAsc.last()) {
+                in 0..9->{println(1)}
+                in 10..19->{println(2)}
+                else -> {println(3)}
+            }
+
+        } catch (f : EOFException ) {
+            break
+        } catch (n :EOFException ) {
+            break
+        } catch (e :Exception) {
+            break
+        }
+    }
 }
