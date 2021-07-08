0aa60e8035c4ac85c761b68d465a3a2bbde9ef01
Jeferson Melo <jefsdemelo@gmail.com>
2021-07-08 13:27:04 -0300

    Triângulo concluído de duas formas

--- 09-Santander-Bootcamp-Mobile-Developer/12-Solucionando-desafios-em-Kotlin/04-Triângulo/src/main/kotlin/main.kt
+++ 09-Santander-Bootcamp-Mobile-Developer/12-Solucionando-desafios-em-Kotlin/04-Triângulo/src/main/kotlin/main.kt
@@ -1,3 +1,32 @@
+import java.io.Console
+
 fun main(args: Array<String>) {
-    println("Hello World!")
+
+    fun Double.format(digits: Int) = "%.${digits}f".format(this).replace(',', '.')
+
+    val (a, b, c) = readLine()!!.split(" ").map { it.toDouble() }
+    val result: Double
+
+    //insira as variaveis de acordo com o calculo do perimetro e area
+
+    // 2º
+    if (a < (b + c) && b < (a + c) && c < (a + b))
+    {
+        result = a + b + c;
+        println("Perimetro = ${result.format(1)}")
+    }
+    else
+    {
+        result = (a + b) * c / 2.0;
+        println("Area = ${result.format(1)}")
+    }
+
+    /* 1ª
+    val p = (a + b + c).format(1)
+
+    val r = (((a + b) * c / 2)).format(1)
+
+    if (a < (b + c) && b < (a + c) && c < (a + b)) println("Perimetro = $p")
+    else println("Area = $r")
+    */
 }
