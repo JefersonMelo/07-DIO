d5bbd8cc8c0baecd4bcfae2ff102b80d0a849500
Jeferson Melo <jefsdemelo@gmail.com>
2021-07-13 10:43:54 -0300

    Desafio Contagem das Cédulas concluído

--- 09-Santander-Bootcamp-Mobile-Developer/14-Praticando-programação-com-Kotlin/03-Contagem-de-Cédulas/src/main/kotlin/main.kt
+++ 09-Santander-Bootcamp-Mobile-Developer/14-Praticando-programação-com-Kotlin/03-Contagem-de-Cédulas/src/main/kotlin/main.kt
@@ -1,3 +1,16 @@
+import java.util.Scanner
+
 fun main(args: Array<String>) {
-    println("Hello World!")
-}
+    val input = Scanner(System.`in`)
+
+    var valor = input.nextInt()
+    println(valor)
+
+    val cedulas = arrayOf(100, 50, 20, 10, 5, 2, 1)
+
+    for (nota in cedulas){
+        var qtdCedulas = valor / nota
+        println("${qtdCedulas} nota(s) de R$ ${nota},00");
+        valor %= nota
+    }
+}  
