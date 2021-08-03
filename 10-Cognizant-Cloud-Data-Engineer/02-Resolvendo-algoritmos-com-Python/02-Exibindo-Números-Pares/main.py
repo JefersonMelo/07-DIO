cdac98255c0d24a99264ba67251070d8a855c1a6
Jeferson Melo <jefsdemelo@gmail.com>
2021-08-03 11:09:35 -0300

    commit inicial

---
+++ 10-Cognizant-Cloud-Data-Engineer/02-Resolvendo-algoritmos-com-Python/02-Exibindo-Números-Pares/main.py
@@ -0,0 +1,23 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+def exibindo_numeros_pares():
+    n = int(input())
+    for i in range(n + 1):
+        if (i % 2) == 0 and i > 0:
+            print(i)
